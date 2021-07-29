fce7ea9d65800737d3b560346dcf1cbf65bacb09
James D <71669407+JamesAtTensure@users.noreply.github.com>
2021-07-29 08:36:18 -0600

    fixed spacing

--- driver/driver.go
+++ driver/driver.go
@@ -1,16 +1,16 @@
 package driver
 
 import (
-	"database/sql"
-	"fmt"
+    "database/sql"
+    "fmt"
 
-	_ "github.com/go-sql-driver/mysql"
+    _ "github.com/go-sql-driver/mysql"
 )
 
 // DB ...
 type DB struct {
-	SQL *sql.DB
-	// Mgo *mgo.database
+    SQL *sql.DB
+    // Mgo *mgo.database
 }
 
 // DBConn ...
@@ -18,18 +18,18 @@ var dbConn = &DB{}
 
 // ConnectSQL ...
 func ConnectSQL(host, port, uname, pass, dbname string) (*DB, error) {
-	dbSource := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?charset=utf8",
-		uname,
+    dbSource := fmt.Sprintf(
+        "%s:%s@tcp(%s:%s)/%s?charset=utf8",
+        uname,
         pass,
-		host,
-		port,
-		dbname,
-	)
-	d, err := sql.Open("mysql", dbSource)
-	if err != nil {
-		panic(err)
-	}
-	dbConn.SQL = d
-	return dbConn, err
+        host,
+        port,
+        dbname,
+    )
+    d, err := sql.Open("mysql", dbSource)
+    if err != nil {
+        panic(err)
+    }
+    dbConn.SQL = d
+    return dbConn, err
 }
